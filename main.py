1e725fccfb0d7cf17b480df67ff8b3d49c763fc0
lusinetumolabs <brasileira@yandex.ru>
2024-11-28 21:34:00 +0400

    Craps game pythonProject.iml

--- main.py
+++ main.py
@@ -9,34 +9,39 @@ def roll_dice():
 # Function to play the game
 def play_craps():
     print("Welcome to the Craps game!")
-    input("Press Enter to roll the dice...")
-
-    # First roll
-    roll = roll_dice()
-    print(f"You rolled: {roll}")
-
-    # Check first roll outcomes
-    if roll == 7 or roll == 11:
-        print("You win! You rolled a 7 or 11.")
-        return
-    elif roll == 2 or roll == 3 or roll == 12:
-        print("Casino wins! You rolled a 2, 3, or 12 (craps).")
-        return
-    else:
-        print(f"Your goal number is now {roll}. Try to roll this again!")
-        goal = roll
-
-    # Keep rolling until the player wins or loses
     while True:
-        input("Press Enter to roll again...")
+        input("Press Enter to roll the dice...")
+
+        # First roll
         roll = roll_dice()
         print(f"You rolled: {roll}")
 
-        if roll == goal:
-            print(f"You rolled your goal number {goal}. You win!")
-            break
-        elif roll == 7:
-            print("You rolled a 7. You lose!")
+        # Check first roll outcomes
+        if roll == 7 or roll == 11:
+            print("Yay, you win! 🎉 You rolled a 7 or 11! Well done!")
+        elif roll == 2 or roll == 3 or roll == 12:
+            print("Oops, you lost! 😞 You rolled a 2, 3, or 12 (craps). Better luck next time!")
+        else:
+            print(f"Your goal number is now {roll}. Try to roll this again! Let's go!")
+            goal = roll
+
+            # Keep rolling until the player wins or loses
+            while True:
+                input("Press Enter to roll again...")
+                roll = roll_dice()
+                print(f"You rolled: {roll}")
+
+                if roll == goal:
+                    print(f"Congratulations! 🎉 You rolled your goal number {goal}. You win!")
+                    break
+                elif roll == 7:
+                    print("Oh no, you rolled a 7! 😟 You lose. Don't worry, you'll get 'em next time!")
+                    break
+
+        # Ask if the player wants to play again
+        play_again = input("Do you want to play again? (y/n): ").strip().lower()
+        if play_again != 'y':
+            print("Thanks for playing! 🎲 See you next time! 👋")
             break
 
 # Start the game
